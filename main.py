bb5659d1693636aa835b88e9e004229567638dd2
Torng <cool870427@gmail.com>
2022-10-13 14:20:52 +0800

    use w gan

--- main.py
+++ main.py
@@ -44,90 +44,69 @@ dl = DataLoader(preprocess.whole_training_data, batch_size=64, shuffle=True, dro
 print("Starting Training Loop...")
 # For each epoch
 save_name = 1
+
+
+def gradient_penalty(critic, real_image, fake_image, device=None):
+    batch_size, channel, height, width = real_image.shape
+    fake_image = fake_image.to(device)
+    # alpha is selected randomly between 0 and 1
+    alpha = torch.rand(batch_size, 1, 1, 1, device=device).repeat(1, channel, height, width)
+    # interpolated image=randomly weighted average between a real and fake image
+    # interpolated image ← alpha *real image  + (1 − alpha) fake image
+    interpolatted_image = (alpha * real_image) + (1 - alpha) * fake_image
+    interpolatted_image = interpolatted_image.to(device)
+    # calculate the critic score on the interpolated image
+    interpolated_score = critic(interpolatted_image)
+
+    # take the gradient of the score wrt to the interpolated image
+    gradient = torch.autograd.grad(inputs=interpolatted_image,
+                                   outputs=interpolated_score,
+                                   retain_graph=True,
+                                   create_graph=True,
+                                   grad_outputs=torch.ones_like(interpolated_score, device=device)
+                                   )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+
+
+
+iterator = iter(dl)
 for epoch in range(num_epochs):
     # For each batch in the dataloader
-    for i, data in enumerate(dl, 0):
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
-        ## Train with all-real batch
-        d_net.zero_grad()
-        # Format batch
-        real_cpu = data.to(device)
+    for _ in range(5):  # train Dnet 5 times
+        data = next(iterator)
+        real_cpu = data[0].to(device)
         b_size = real_cpu.size(0)
-        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
-        # Forward pass real batch through D
-        output = d_net(real_cpu).view(-1)
-        # if i % 16 == 0:
-        errD_real = criterion(output, label)
-        errD_real.backward()
-        # else:
-        #     errD_real = torch.tensor(0)
-        D_x = output.mean().item()
-
-        ## Train with all-fake batch
-        # Generate batch of latent vectors
-        noise = torch.randn(b_size, 128, 1, 1, device=device)
-        # Generate fake image batch with G
-        fake = g_net(noise)
-        label.fill_(fake_label)
-        # Classify all fake batch with D
-        output = d_net(fake.detach()).view(-1)
-        # Calculate D's loss on the all-fake batch
-        errD_fake = criterion(output, label)
-        # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-        errD_fake.backward()
-        D_G_z1 = output.mean().item()
-        # Compute error of D as sum over the fake and the real batches
-        errD = errD_real + errD_fake
-        # Update D
-        # if i % 16 == 0:
+        predr = d_net(real_cpu).view(-1)
+        # maximize predr, therefore minus sign
+        lossr = predr.mean()
+        z = torch.randn(b_size, 128, 1, 1, device=device)
+        xf = g_net(z) # gradient would be passed down
+        predf = d_net(xf)
+        # min predf
+        lossf = predf.mean()
+        loss_D = lossf - lossr  # max
+        gp = gradient_penalty(d_net, real_cpu, xf, device)
+        loss_D = loss_D + gp * 10
+        optimizerD.zero_grad()
+        loss_D.backward()
         optimizerD.step()
+    z = torch.randn(b_size, 128, 1, 1, device=device)
+    xf = g_net(z)
+    predf = d_net(xf)
+    loss_G = -predf.mean()  # min
+    # optimize
+    optimizerG.zero_grad()
+    loss_G.backward()
+    optimizerG.step()
 
-        ############################
-        # (2) Update G network: maximize log(D(G(z)))
-        ###########################
-        g_net.zero_grad()
-        label.fill_(real_label)  # fake labels are real for generator cost
-        # Since we just updated D, perform another forward pass of all-fake batch through D
-        output = d_net(fake).view(-1)
-        # Calculate G's loss based on this output
-        errG = criterion(output, label)
-        # Calculate gradients for G
-        errG.backward()
-        D_G_z2 = output.mean().item()
-        # Update G
-        optimizerG.step()
-
-        # Output training stats
-        if i % 16 == 0:
-            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                  % (epoch, num_epochs, i, len(dl),
-                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-
-        # Save Losses for plotting later
-        G_losses.append(errG.item())
-        D_losses.append(errD.item())
-
-        # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(preprocess.training_data) - 1)):
-            with torch.no_grad():
-                fake = g_net(fixed_noise).detach().cpu()
-            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
-
-        iters += 1
-
-    if epoch % 500 == 0:
+    if epoch % 2 == 0:
+        print("epoch:{0} ==> lossDr:{1}, lossDf:{2}, lossD:{3},lossG:{4}".format(epoch, lossr, lossf, -loss_D, loss_G))
+    if epoch % 10 == 0:
         path = Path("model_set/")
         path.mkdir(exist_ok=True)
         output_path = path / ("model_" + str(epoch))
         torch.save(g_net, output_path)
-
-plt.figure(figsize=(10, 5))
-plt.title("Generator and Discriminator Loss During Training")
-plt.plot(G_losses, label="G")
-plt.plot(D_losses, label="D")
-plt.xlabel("iterations")
-plt.ylabel("Loss")
-plt.legend()
-plt.show()
