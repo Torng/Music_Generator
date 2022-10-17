2f4a150bdf4aecbe85a8f9b2bd7c0e55cdc435c6
Torng <cool870427@gmail.com>
2022-10-17 17:42:50 +0800

    add tuner

--- main.py
+++ main.py
@@ -12,18 +12,13 @@ from midi_utils import denormalize, notes_to_midi
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("using {}".format(device))
-
-num_epochs = 10000
 g_net = Generator().to(device)
 d_net = Discriminator().to(device)
 
 preprocess = Preprocess('maestro-v3.0.0/2011')
 
-iters = 0
 dl = DataLoader(preprocess.whole_training_data, batch_size=32, shuffle=True, drop_last=True)
 print("Starting Training Loop...")
-# For each epoch
-save_name = 1
 
 
 def gradient_penalty(critic, real_image, fake_image, device=None):
@@ -51,6 +46,7 @@ def gradient_penalty(critic, real_image, fake_image, device=None):
     return gradient_penalty
 
 
+@ray.remote(num_gpus=1)
 def train(config):
     iterator = iter(dl)
     alpha = config['alpha'] if config else 10
@@ -114,28 +110,3 @@ def train(config):
         #     music = denormalize(xf, preprocess.midi_std, preprocess.midi_mean)
         #     notes_to_midi(music, str(output_path), preprocess.instrument_name)
     return {'net': g_net, 'loss_d': loss_d_list[-1], 'loss_g': loss_g_list[-1]}
-
-
-search_space = {'d_lr': tune.uniform(0.001, 0.00001),
-                'g_lr': tune.uniform(0.001, 0.00001),
-                'alpha': tune.grid_search([i for i in range(0, 20, 5)]),
-                'epochs': tune.grid_search([100, 300, 500, 1000, 2000]),
-                'train_d': tune.grid_search([i for i in range(0, 20, 5)])}
-
-tuner = tune.Tuner(train, param_space=search_space, tune_config=tune.TuneConfig(
-    num_samples=50
-))
-result = tuner.fit()
-max_loss_d_net = result.get_best_result(metric="loss_d", mode="max").metrics['net']
-max_loss_d = result.get_best_result(metric="loss_d", mode="max").metrics['loss_d']
-min_loss_g = result.get_best_result(metric="loss_d", mode="max").metrics['loss_g']
-print("==========max_loss_d_net==========")
-print("loss_d--->", max_loss_d)
-print("loss_g--->", min_loss_g)
-
-min_loss_g_net = result.get_best_result(metric="loss_g", mode="min").metrics['net']
-max_loss_d = result.get_best_result(metric="loss_g", mode="max").metrics['loss_d']
-min_loss_g = result.get_best_result(metric="loss_g", mode="max").metrics['loss_g']
-print("==========min_loss_d_net==========")
-print("loss_d--->", max_loss_d)
-print("loss_g--->", min_loss_g)
---
+++ tuner.py
@@ -0,0 +1,27 @@
+from multiprocessing import Process, Pool
+from main import train
+import multiprocessing
+import random
+
+
+class Tuner:
+    def tune(self, config):
+        self.result = train(config)
+
+    def create_config(self):
+        config = {'d_lr': random.uniform(0.001, 0.00001),
+                  'g_lr': random.uniform(0.001, 0.00001),
+                  'alpha': random.choice([i for i in range(0, 20, 5)]),
+                  'epochs': random.choice(([100, 300, 500, 1000, 2000])),
+                  'train_d': random.choice([i for i in range(0, 20, 5)])}
+        return config
+
+
+if __name__ == "__main__":
+    random.seed(0)
+    tuner = Tuner()
+    cpu_count = multiprocessing.cpu_count()
+    pool = Pool(cpu_count)
+    results = pool.starmap(tuner.tune, [(tuner.create_config()) for i in range(100)])
+    for result in results:
+        print(result[''])
